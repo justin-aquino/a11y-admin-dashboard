7c2adaca1ffcc826a051a528afb4f91e19ffda21
Justin Aquino <justin.aquino25@gmail.com>
2023-03-09 16:28:19 -0800

    top bar search input and icons

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
+import { Box, IconButton, useTheme } from "@mui/material"
+import { useContext } from "react"
+import { ColorModeContext, tokens } from "../../theme"
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 const TopBar = () => {
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+    const colorMode = useContext(ColorModeContext);
   return (
-    <div>TopBar</div>
+    <Box display="flex" justifyCOntent="space-between" p={2}>
+    <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+        {/* <label htmlFor="searchInput">Search: </label> */}
+        <InputBase sx={{ml: 2, flex: 1}} placeholder="search" id="searchInput"/>
+        <IconButton aria-label="toggle light / dark mode">
+           <LightModeOutlinedIcon /> 
+        </IconButton>
+        <IconButton>
+            <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+            <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+            <PersonOutlinedIcon />
+        </IconButton>
+        <IconButton>
+
+        </IconButton>
+    </Box>
+</Box>
   )
 }
 
